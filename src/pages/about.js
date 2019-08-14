72af21235e9e10f7a5458fe7770ca37334e22a60
Théo <theo.reveillard@wanadoo.fr>
2019-08-14 10:47:26 +0200

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -30,37 +30,37 @@ const About = ({ data }) => (
         <i className="e1a-wave_tone1" />
       </h1>
       <p>
-        I was born in Valence in France, but I tend to be in a lot of difference
-        places around the world.
+        I was born in Valence, France, but I’ve been a lot of different places
+        around the world.
       </p>
       <p>
-        Sometimes, I try to stay at some places and earn experiences from
-        companies and agencies. My first developer job was at{' '}
+        Nevertheless, I stayed at some places and earned professional
+        experiences from companies and agencies. My first developer job was at{' '}
         <Link style={{ color: 'var(--textLink)' }} href="https://cansii.com/">
           Cansii
         </Link>{' '}
-        back in 2013. Then I went to Japan for a whole year and found in love
-        with this country. Back to France, I worked for a press and digital
-        advertising company called{' '}
+        back in 2013. Then I went to Japan for a whole year and immediately fell
+        in love with this country. Back to France, I worked for a press and
+        digital advertising company named{' '}
         <Link
           style={{ color: 'var(--textLink)' }}
           href="https://www.prodigious.com/our_services/prodigiouscloud/"
         >
           Prodigious
         </Link>
-        . In 2018, I flewed to Autralia and southern asia.
+        . Then in 2018 I flewed to Autralia and southern asia later on.
       </p>
       <p>
-        June 2019 was really a turning point for me: I wanted to focus and
-        contribute on the open source world. Since then I enjoy working with web
-        technologies such as React, Gatsby, NodeJS. I also like playing with
-        design technologies such as threeJS, css animations and Adobe editing
-        softwares.
+        June 2019 has undoubtedly been a turning point for me: I would like to
+        focus on and contribute to the world of open source. And since then,
+        I’ve enjoyed working with web technologies such as React, Gatsby,
+        NodeJS. I also like working with design technologies such as threeJS,
+        CSS Animations and Adobe Editing Softwares.
       </p>
       <p>
         For any assigment, you can reach me on{' '}
         <Email style={{ color: 'var(--textLink)' }} to="trvd.protonmail.com" />{' '}
-        or for private matters:{' '}
+        or for private matters,{' '}
         <Email
           style={{ color: 'var(--textLink)' }}
           to="reveillard.theo@gmail.com"
